4fd9e574ad2a05c1101905aa321b2f850ed23365
charlings <charlings9@gmail.com>
2026-08-18 23:02:41 +0100

    Day 2 assignments

---
+++ assignment-02.py
@@ -0,0 +1,17 @@
+def two_sum(nums, target):
+    # Dictionary to store numbers and their indices
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+# Example 1
+print(two_sum([2,7,11,15], 9))   # Output: [0, 1]
+
+# Example 2
+print(two_sum([3,2,4], 6))       # Output: [1, 2]
+
+# Example 3
+print(two_sum([3,3], 6))         # Output: [0, 1]
---
+++ list.py
@@ -0,0 +1,17 @@
+# shopping_cart = ["car", "food", "apples", "12", "14", "29", "23.0", 
+#     ["parle-g", "fibre-active"], {},]
+
+# print(type(shopping_cart)[0].upper())
+
+# print(shopping_cart[7][1].upper())   # FIBRE-ACTIVE
+# print(f"austine i just bought a {shopping_cart[0].upper()} and {shopping_cart[7][1].upper()}")
+
+# for element in shopping_cart:
+#     print(f"Element: {element}, Type: {type(element)}")
+
+    
+for number in range(1, 6):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
